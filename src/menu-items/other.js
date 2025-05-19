b2c345203966020297f10ae83b4db24d3438cf3b
Mert Karatas <mkaratas@genbil.com>
2025-05-19 22:15:59 +0300

    latest

--- src/menu-items/other.js
+++ src/menu-items/other.js
@@ -10,15 +10,7 @@ const other = {
   id: 'sample-docs-roadmap',
   type: 'group',
   children: [
-    {
-      id: 'documentation',
-      title: 'Documentation',
-      type: 'item',
-      url: 'https://www.google.com',
-      icon: icons.IconHelp,
-      external: true,
-      target: true
-    }
+   
   ]
 };
 
